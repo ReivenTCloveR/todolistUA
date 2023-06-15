04fb0fff82275c2135af8e03f2ace3220aee7936
ReivenTCloveR <127988053+ReivenTCloveR@users.noreply.github.com>
2023-06-15 16:03:05 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 /* Importar App externo */
+
+/*Hugo Valdes*/
 import {App} from './App'
 
 const root = document.getElementById('root')
